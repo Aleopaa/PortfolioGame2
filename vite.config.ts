c9e3ae3ca418bf915d42013273e3382c762ce1d3
Aleksandra Opacic <aleksandraopacic@hotmail.co.uk>
2024-12-05 16:02:50 +0000

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { glslify } from "vite-plugin-glslify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/PortfolioGame2/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), glslify()],
   assetsInclude: ["**/*.glb"],
 });
